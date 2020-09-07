3f3a6b150464e502e6bd297be7798abc45d4c3ae
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2020-09-08 01:09:47 +0530

    restauarnt changes

--- HotelManagement/src/com/service/RestaurantImpl.java
+++ HotelManagement/src/com/service/RestaurantImpl.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.model.Item;
+import com.model.Table;
 import com.util.DBConnection;
 
 public class RestaurantImpl implements IRestaurant {
@@ -24,11 +25,11 @@ public class RestaurantImpl implements IRestaurant {
 		
 		try {
 			connection=DBConnection.initializedb();
-			pt=connection.prepareStatement("insert into Item(ItemNo,Name,Category,Price) values(?,?,?,?)");
-			pt.setInt(1, item.getItemno());
-			pt.setString(2, item.getName());
-			pt.setString(3, item.getCategory());
-			pt.setFloat(4, item.getPrice());
+			pt=connection.prepareStatement("insert into Item(Name,Category,Price) values(?,?,?)");
+		
+			pt.setString(1, item.getName());
+			pt.setString(2, item.getCategory());
+			pt.setFloat(3, item.getPrice());
 			
 			pt.execute();
 			
@@ -43,22 +44,7 @@ public class RestaurantImpl implements IRestaurant {
 		
 	}
 
-	@Override
-	public void generateItemid() {
-		// TODO Auto-generated method stub
-		
-		try {
-			connection=DBConnection.initializedb();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-	}
+	
 
 	@Override
 	public ArrayList<Item> listitems() {
@@ -152,11 +138,12 @@ public class RestaurantImpl implements IRestaurant {
 		
 		try {
 			connection=DBConnection.initializedb();
-			pt=connection.prepareStatement("update Item set ItemNo=?,Name=?,Category=?,Price=?");
-			pt.setInt(1, item.getItemno());
-			pt.setString(2, item.getName());
-			pt.setString(3, item.getCategory());
-			pt.setFloat(4, item.getPrice());
+			pt=connection.prepareStatement("update Item set Name=?,Category=?,Price=? where ItemNO=?");
+		
+			pt.setString(1, item.getName());
+			pt.setString(2, item.getCategory());
+			pt.setFloat(3, item.getPrice());
+			pt.setInt(4, item.getItemno());
 			
 			pt.executeUpdate();
 			
@@ -170,6 +157,150 @@ public class RestaurantImpl implements IRestaurant {
 		
 		
 		
+		
+	}
+
+
+
+	@Override
+	public void addTable(Table table) {
+		// TODO Auto-generated method stub
+		
+		try {
+			connection=DBConnection.initializedb();
+			pt=connection.prepareStatement("insert into Tables(Type,Capacity,Price) values(?,?,?) ");
+			pt.setString(1, table.getType());
+			pt.setInt(2, table.getCapacity());
+			pt.setFloat(3, table.getPrice());
+			pt.execute();
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+
+
+	@Override
+	public ArrayList<Table> listtables() {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Table> tables=new ArrayList<Table>();
+		
+		
+		try {
+			connection=DBConnection.initializedb();
+			pt=connection.prepareStatement("select * from tables");
+			ResultSet result=pt.executeQuery();
+			
+			
+			while(result.next()) {
+				
+				Table table=new Table();
+				table.setTableno(result.getInt(1));
+				table.setType(result.getString(2));
+				table.setCapacity(result.getInt(3));
+				table.setPrice(result.getFloat(4));
+				
+				tables.add(table);
+				
+			}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tables;
+	}
+
+
+
+	@Override
+	public void deleteTable(int tableno) {
+		// TODO Auto-generated method stub
+		
+		try {
+			connection=DBConnection.initializedb();
+			pt=connection.prepareStatement("delete from Tables where TableNO=?");
+			pt.setInt(1,tableno);
+			pt.execute();
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+
+
+	@Override
+	public Table getTable(int tableno) {
+		// TODO Auto-generated method stub
+		
+		Table table=new Table();
+		
+		try {
+			connection=DBConnection.initializedb();
+			pt=connection.prepareStatement("select * from tables where TableNO=?");
+			pt.setInt(1, tableno);
+			ResultSet result=pt.executeQuery();
+			
+			
+		    table.setTableno(result.getInt(1));
+		    table.setType(result.getString(2));
+			table.setCapacity(result.getInt(3));
+			table.setPrice(result.getFloat(4));
+				
+			
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return table;
+	}
+
+
+
+	@Override
+	public void updateTable(Table table) {
+		// TODO Auto-generated method stub
+		
+		try {
+			connection=DBConnection.initializedb();
+			pt=connection.prepareStatement("update Tables set Type=?,Capacity=?,Price=? where TableNo=?");
+			pt.setString(1,table.getType());
+			pt.setInt(2, table.getCapacity());
+			pt.setFloat(3, table.getPrice());
+			pt.setInt(4, table.getTableno());
+			
+			pt.executeUpdate();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
